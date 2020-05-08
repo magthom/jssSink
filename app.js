95ee95b907302ade62c1b716fbcf7c616128e7f7
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-08 15:21:11 -0500

    Hello World Alert

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-var name = "Maggie";
-
-const amount = 50;
-
-var val = 5 + 4;
-
-// displays Maggie
-    console.log(name); 
-// displays 50
-    console.log(amount);
-// displays the sum
-    console.log(val);
+alert("Hello World!");
