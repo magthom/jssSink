71a1dfbb5ac1181db2e8ab17780ce7d0af242eb4
Maggie Thompson <maggiethompson@Maggies-MacBook-Air.local>
2020-05-08 13:40:44 -0500

    added comments

---
+++ app.js
@@ -0,0 +1,12 @@
+var name = "Maggie";
+
+const amount = 50;
+
+var val = 5 + 4;
+
+// displays Maggie
+    console.log(name); 
+// displays 50
+    console.log(amount);
+// displays the sum
+    console.log(val);
